596c7463f8eda07a3b1c573a918d0171426ca2f9
Shweta R Kumthekar <srk77@afsconnect1.njit.edu>
2017-11-15 20:52:19 -0500

    select find

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+define('DATABASE', 'srk77');
+define('USERNAME', 'srk77');
+define('PASSWORD', 'shweta13');
+define('CONNECTION', 'sql2.njit.edu');
+class dbConn{
+    //variable to hold connection object.
+    protected static $db;
+    //private construct - class cannot be instatiated externally.
+    private function __construct() {
+        try {
+            // assign PDO object to db variable
+            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $e) {
+            //Output error - would normally log this to error file rather than output to user.
+            echo "Connection Error: " . $e->getMessage();
+        }
+    }
+    // get connection function. Static method - accessible without instantiation
+    public static function getConnection() {
+        //Guarantees single instance, if no connection object exists then create one.
+        if (!self::$db) {
+            //new connection object.
+            new dbConn();
+        }
+        //return connection.
+        return self::$db;
+    }
+}
+class collection 
+{
+    static public function create() {
+      $model = new static::$modelName;
+      return $model;
+    }
+    static public function findAll() {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        return $recordsSet;
+    }
+    static public function findOne($id) {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        return $recordsSet[0];
+    }
+}
+
+class accounts extends collection {
+    protected static $modelName = 'account';
+}
+class todos extends collection {
+    protected static $modelName = 'todo';
+}
+
+$records = todos::findOne(2);
+print_r($records);
+
+?>
